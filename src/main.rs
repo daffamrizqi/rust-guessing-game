811ffeae6f9e37d1d6c4701ec35bcb1bf3e8469f
DaffaRizqi <daffarizqi@pop-os.localdomain>
2023-01-17 13:41:05 +0700

    finished

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the Number!");
+
+    // 4
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess!");
+
+        // 1
+        let mut guess = String::new();
+
+        // 2
+        io::stdin()
+            .read_line(&mut guess)
+            // 3
+            .expect("Failed to read guessing!");
+
+        // 6
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        // 5
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
